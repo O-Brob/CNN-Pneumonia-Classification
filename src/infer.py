84b052d36a4fbd81e7d671fa555ebb2f477b89d9
O-Brob <163575083+O-Brob@users.noreply.github.com>
2026-05-14 17:26:41 +0200

    Initial stem of infer.py

---
+++ src/infer.py
@@ -0,0 +1,8 @@
+# ===== Imports ===== #
+import torch
+from torch import nn
+# ===== Inference methods ===== #
+
+# TODO: Implement Inference on single image, using the trained model.
+def model_infer(model: nn.Module) -> None:
+    pass
